8b1b44b286263b114862b2f540b82a6ff8331c54
Jason Rigden <jasonrigden@gmail.com>
2016-01-11 15:32:02 -0800

    ?

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='pyloudness',
+      version='1.0',
+      description='Find out how loud that file is',
+      url='https://github.com/jrigden/pyloudness',
+      author='Jason Rigden',
+      author_email='jasonrigden@gmail.com',
+      license='MIT',
+      packages=['pyloudness'],
+      zip_safe=False)
