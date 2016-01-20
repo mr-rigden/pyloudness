cda7524972a5a247c6701c7dbdf323d3e81a3d73
J. M. Rigden <jrigden@users.noreply.github.com>
2016-01-20 09:42:51 -0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,6 +2,8 @@ from setuptools import setup, find_packages
 from codecs import open
 from os import path
 
+here = path.abspath(path.dirname(__file__))
+
 with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
     long_description = f.read()
 
